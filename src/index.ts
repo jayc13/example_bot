59c027401ace273e6aa8f0e8c8e9540d0133b210
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2021-09-24 01:47:46 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,6 @@ export = (app: Application) => {
     app.on('pull_request.opened', async (context: Context) => {
         const repo = context.payload.repository.name;
         const owner = context.payload.repository.owner.login;
-        await context.github.pullRequests.update({
-            owner,
-            repo,
-            number: context.payload.number,
-            state: 'closed'
-        })
     })
 }
 
